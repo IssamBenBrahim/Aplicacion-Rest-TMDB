25663f1974669a051c6b58a3b57328d83c4bf36e
Issam <issambenbrahim2001@gmail.com>
2021-06-06 17:33:15 +0200

    Asignacion del metodo GetAll() en la clase PeliculasManager para poder hacer consultas sobre las peliculas

--- RestTMDB/RestTMDB/Data/PeliculasManager.cs
+++ RestTMDB/RestTMDB/Data/PeliculasManager.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RestTMDB.Data
 {
     class PeliculasManager
     {
+        public static readonly string apikey = "34c6606ba0b79146cfef27a886248445";
+
+        public async Task<Root> GetAll(string titulo)
+        {
+            string Url = "https://api.themoviedb.org/3/search/movie?api_key=" + apikey + "&query=" + titulo;
+
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync(Url);
+            return JsonConvert.DeserializeObject<Root>(result);
+
+        }
     }
 }
